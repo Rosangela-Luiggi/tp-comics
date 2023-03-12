c2a182ce9eb2f5a65f465d6b7eaef1d953ebd14c
Rosangela-Luiggi <rosangelaluiggim@gmail.com>
2023-03-11 23:08:51 -0300

    filtrado por location y character

--- app.js
+++ app.js
@@ -6,16 +6,18 @@ const $ = (elemento) => document.querySelector(elemento);
 
 window.addEventListener("load", function () {
     const $containerCards = $(".container-cards");
-    
+
     let page = 1;
     let nameAll = "";
-    let garder = "";
+    let genderAll = "";
+    let statusAll = "";
+    let type = "character"
 
     /* let pageTotal =  */
 
     //filter
-    const $filterGerder = $("#select-gender");
-    const $filterLocation = $("#select-location");
+    const $filterGender = $("#select-gender");
+    const $filterType = $("#select-type");
     const $filterStatus = $("#select-status");
     const $firterOrder = $("#select-order");
     const $inptSearch = $("#ipt-search");
@@ -32,43 +34,54 @@ window.addEventListener("load", function () {
     /* ----------------------------------------------------Pintar las cards------------------------------------------------------------------- */
 
     const gethApi = (url) => {
-        fetch(`${url}${"/?page=" + page}${nameAll}${garder}`)
+        fetch(`${url}${type}${"/?page=" + page}${nameAll}${genderAll}${statusAll}`)
             .then(response => response.json())
             .then(info => {
 
                 paintCards(info);
+
+
             })
             .catch(error => console.log(error))
     }
 
 
-    gethApi("https://rickandmortyapi.com/api/character");
+    gethApi("https://rickandmortyapi.com/api/");
+
+
 
     const paintCards = (array) => {
         $containerCards.innerHTML = "";
         array.results.forEach(elem => {
-            $containerCards.innerHTML += `<div class="card">
-            <img src=${elem.image}>
+            if(type = "location"){
+                $containerCards.innerHTML += `<div class="card">
+            <img src=${elem.residents.image}>
             <div>
             <p id="name">${elem.name}</p>
-            <span>${elem.species}</span>
-            <p><span>Location: <br></span>${elem.location.name}</p>
-            <p><span>Status:<br></span>${elem.status}</p></div>
+            <span>${elem.type}</span>
+            <p><span>Dimensión:<br></span>${elem.dimension}</p></div>
         </div> `
+            }else{
+                $containerCards.innerHTML += `<div class="card">
+                <img src=${elem.image}>
+                <div>
+                <p id="name">${elem.name}</p>
+                <p><span>Status:<br></span>${elem.status}</p></div>
+            </div> `
+            }
+           
         });
     }
 
-    /*  let paintResul = ()=>{
-         $("#totalResult").innerText += `${total}`
- 
-     }
- paintResul() */
+//
+
+
     /* --------------------------------------------------Paginado---------------------------------------------- */
 
     $btnFirst.addEventListener("click", () => {
         if (page = 1) {
             page = 1;
-            gethApi("https://rickandmortyapi.com/api/character");
+            gethApi("https://rickandmortyapi.com/api/");
         }
     });
 
@@ -90,7 +103,7 @@ window.addEventListener("load", function () {
         }
     });
 
-// boton de ultima pagina
+    // boton de ultima pagina
     $btnLast.addEventListener("click", () => {
         if (page = 42) {
             page = 42;
@@ -98,32 +111,60 @@ window.addEventListener("load", function () {
         }
     });
 
-//filtrado por input
-    $btnFilter.addEventListener("click",()=>{
-        if($inptSearch.value === ""){
+    //filtrado por input
+    $btnFilter.addEventListener("click", () => {
+        if ($filterType.value === "character") {
+            type = "character"
+        } else {
+            type = "location"
+
+        }
+
+        if ($inptSearch.value === "") {
             nameAll = ""
-        }else{
-        nameAll = `&name=${$inptSearch.value.toLowerCase()}`
-        console.log($inptSearch.value)
-        console.log(nameAll)
+        } else {
+            nameAll = `&name=${$inptSearch.value.toLowerCase()}`
+            console.log($inptSearch.value)
+            console.log(nameAll)
         }
-        gethApi("https://rickandmortyapi.com/api/character");
+        gethApi("https://rickandmortyapi.com/api/");
     });
 
-//filtrar por genero
-    $filterGerder.addEventListener("blur",()=>{
-        if($filterGerder.value === ""){
-            garder = ""
-        }else{
-        garder = `&gender=${$filterGerder.value.toLowerCase()}`
-        
-       
+    //filtrar por genero
+    $filterGender.addEventListener("click", () => {
+        if ($filterGender.value === "") {
+            genderAll = ""
+        } else {
+            genderAll = `&gender=${$filterGender.value.toLowerCase()}`
+
+
         }
-        gethApi("https://rickandmortyapi.com/api/character");
+        gethApi("https://rickandmortyapi.com/api/");
     });
 
-    
-    
+    //filtrado por status
+    $filterStatus.addEventListener("click", () => {
+        if ($filterStatus.value === "") {
+            statusAll = ""
+        } else {
+            statusAll = `&status=${$filterStatus.value.toLowerCase()}`
+
+        }
+        gethApi("https://rickandmortyapi.com/api/");
+
+    });
+
+    /* ----------------------------------------------------filtro type----------------------------------------------------------- */
+    /*   $filterType.addEventListener("click", () => {
+          if ($filterType.value === "location") {
+              
+          }if($filterType.value === "character"){
+              
+              gethApi("https://rickandmortyapi.com/api/")
+              console.log($filterType.value)
+          }
+      }) */
+
 
 });
 
